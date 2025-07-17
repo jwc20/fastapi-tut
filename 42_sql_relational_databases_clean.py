9ab86329b49f6f37b00ad2abafce4960d6c75983
jwc20 <choi.jae100@gmail.com>
2025-07-18 08:28:00 +0900

    42-43

--- 42_sql_relational_databases_clean.py
+++ 42_sql_relational_databases_clean.py
@@ -1,15 +1,7 @@
-from contextlib import asynccontextmanager
 from typing import Annotated
 
-from fastapi import FastAPI, Depends, HTTPException, Query
+from fastapi import Depends, FastAPI, HTTPException, Query
 from sqlmodel import Field, Session, SQLModel, create_engine, select
-
-
-# class Hero(SQLModel, table=True):
-#     id: int | None = Field(default=None, primary_key=True)
-#     name: str = Field(index=True)
-#     age: int | None = Field(default=None, index=True)
-#     secret_name: str
 
 
 class HeroBase(SQLModel):
@@ -27,16 +19,10 @@ class HeroPublic(HeroBase):
 
 
 class HeroCreate(HeroBase):
-    """
-    This is how you would handle passwords. Receive them, but don't return them in the API.
-    You would also hash the values of the passwords before storing them, never store them in plain text.
-    """
-
     secret_name: str
 
 
 class HeroUpdate(HeroBase):
-    # class HeroUpdate():
     name: str | None = None
     age: int | None = None
     secret_name: str | None = None
@@ -49,60 +35,31 @@ connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, connect_args=connect_args)
 
 
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
 def get_session():
     with Session(engine) as session:
         yield session
 
 
 SessionDep = Annotated[Session, Depends(get_session)]
+app = FastAPI()
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
+@app.on_event("startup")
+def on_startup():
     create_db_and_tables()
-    yield
-    print("shutting down")
 
 
-app = FastAPI(lifespan=lifespan)
-
-
-# Deprecated
-# app = FastAPI()
-# @app.on_event("startup")
-# def on_startup():
-#     create_db_and_tables()
-
-
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
-
-
-# @app.post("/heroes/")
-# def create_hero(hero: Hero, session: SessionDep) -> Hero:
-#     session.add(hero)
-#     session.commit()
-#     session.refresh(hero)
-#     return hero
-
-
-@app.post("/heroes/")
+@app.post("/heroes/", response_model=HeroPublic)
 def create_hero(hero: HeroCreate, session: SessionDep):
     db_hero = Hero.model_validate(hero)
     session.add(db_hero)
     session.commit()
     session.refresh(db_hero)
     return db_hero
-
-
-# @app.get("/heroes/")
-# def read_heroes(
-#     session: SessionDep,
-#     offset: int = 0,
-#     limit: Annotated[int, Query(le=100)] = 100,
-# ):
-#     heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
-#     return heroes
 
 
 @app.get("/heroes/", response_model=list[HeroPublic])
@@ -113,14 +70,6 @@ def read_heroes(
 ):
     heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
     return heroes
-
-
-# @app.get("/heroes/{hero_id}")
-# def read_hero(hero_id: int, session: SessionDep) -> Hero:
-#     hero = session.get(Hero, hero_id)
-#     if not hero:
-#         raise HTTPException(status_code=404, detail="Hero not found")
-#     return hero
 
 
 @app.get("/heroes/{hero_id}", response_model=HeroPublic)
